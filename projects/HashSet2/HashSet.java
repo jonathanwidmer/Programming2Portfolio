ae47e3cb3986ec0c4ab59eb8634396fe47db555e
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2021-05-28 12:25:31 -0600

    Add files via upload

--- projects/HashSet2/HashSet.java
+++ projects/HashSet2/HashSet.java
@@ -1,1 +1,14 @@
+import java.util.HashSet;
 
+public class hashSet{
+  public static void main (String[] args){
+    Set<String> words = new HashSet<>();
+    words.add("car");
+    words.add("boat");
+    words.add("plane");
+    words.add("train");
+    words.add("bike");
+    System.out.println(words);
+  }
+
+}
