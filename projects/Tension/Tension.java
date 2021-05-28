e73f3316b55fceef80a49dfec7275b6c37e605b5
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2021-05-28 11:22:51 -0600

    Add files via upload

--- projects/Tension/Tension.java
+++ projects/Tension/Tension.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class Tension{
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int mass = 2;
+    System.out.println("Enter the speed you want the weight to move: ");
+    double speed = in.nextDouble();
+    double tension = (mass * (speed*speed))/3;
+    System.out.println("Tension = " + tension + " newtons");
+    if (tension >= 60){
+      System.out.println("The rope will break");
+    }
+    else{
+      System.out.println("The rope won't break");
+    }
+  }
+}
