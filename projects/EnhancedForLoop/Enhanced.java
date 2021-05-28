5ad9028a4fab1ecfbd183e4095e03b9eba401db1
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2021-05-28 12:17:01 -0600

    Add files via upload

--- projects/EnhancedForLoop/Enhanced.java
+++ projects/EnhancedForLoop/Enhanced.java
@@ -1,1 +1,26 @@
+import java.util.Random;
 
+public class Enhanced{
+  public static void main(String []args){
+    Random rand = new Random();
+    int[] values = new int[100];
+    for (int i = 0; i<values.length; i++){
+      values[i] = rand.nextInt(100+99) - 99;
+      System.out.print(values[i] + " ");
+    }
+    int max=0;
+    for(int element : values){
+      if(max<element){
+        max = element;
+        System.out.println(max);
+      }
+    }
+    int count = 0;
+    for(int element : values){
+      if(element<0){
+        count++;
+        System.out.println(count);
+      }
+    }
+  }
+}
